1109ba459936a66112aaa27fc272a7d83ae5765a
Leonard Tulipan <leo@leotulipan.at>
2023-03-20 17:17:37 +0100

    calling active card but sharebutton not working

--- trello-toolbox-tonic/content-script.js
+++ trello-toolbox-tonic/content-script.js
@@ -1,42 +1,60 @@
-(function() {
-    // 1. Click the share button
-    const shareButton = document.querySelector('.button-link.js-more-menu');
-    if (shareButton) {
-      shareButton.click();
-    } else {
-      console.error('Share button not found');
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  
+async function main() {
+    console.log("Trello Toolbox Tonic called");
+    const activeCard = document.querySelector('.active-card');
+    const closeCardSelector = '.icon-md.icon-close.dialog-close-button.js-close-window';
+  
+    if (activeCard) {
+      console.log("There is an active card");
+      activeCard.click();
+      await sleep(1000); // wait for the card to open
+  
+      // Check if the share button is visible; if not, wait for another 1000ms
+      let shareButton = document.querySelector('.button-link.js-more-menu');
+      if (!shareButton) {
+        await sleep(1000);
+        shareButton = document.querySelector('.button-link.js-more-menu');
+      }
     }
   
-    setTimeout(() => {
-      // 2. Check if the pop-over is shown
-      const popOver = document.querySelector('.pop-over.is-shown');
-      if (popOver) {
-        // 3. Get the value from input with class "js-email"
-        const emailInput = popOver.querySelector('.js-email');
-        if (emailInput) {
-          const emailValue = emailInput.value;
-          
-          // 4. Console log the value and copy it to the clipboard
-          console.log('Email value:', emailValue);
-          navigator.clipboard.writeText(emailValue).then(() => {
-            console.log('Email value copied to clipboard');
-          }).catch(err => {
-            console.error('Failed to copy email value to clipboard:', err);
-          });
-          
-          // 5. Close the pop-over
-          const closeButton = popOver.querySelector('.pop-over-header-close-btn');
-          if (closeButton) {
-            closeButton.click();
-          } else {
-            console.error('Close button not found');
-          }
-        } else {
-          console.error('Email input not found');
+    if (shareButton) {
+        console.log("Clicking sharebutton");
+        shareButton.click();
+        elapsedTime = 0;
+
+        let emailInput = document.querySelector('.pop-over input.js-email');
+        // Wait for the email input to become visible or reach the maximum wait time
+        while (!emailInput && elapsedTime < maxWaitTime) {
+            await sleep(250);
+            elapsedTime += 250;
+            emailInput = document.querySelector('.pop-over input.js-email');
         }
-      } else {
-        console.error('Pop-over not found or not shown');
-      }
-    }, 1000);
-  })();
+
+        if (emailInput) {
+            const emailValue = emailInput.value;
+            console.log(emailValue);
+            navigator.clipboard.writeText(emailValue);
+        
+            const closeButton = document.querySelector('.pop-over-header-close-btn.icon-sm.icon-close');
+            if (closeButton) {
+                closeButton.click();
+            }
+        
+            if (activeCard) {
+                const closeOpenedCardButton = document.querySelector(closeCardSelector);
+                if (closeOpenedCardButton) {
+                closeOpenedCardButton.click();
+                }
+            }
+            } else {
+            console.error('Trello Toolbox Tonic: Email input not found.');
+            }
+    } else {
+        console.error('Trello Toolbox Tonic: Share button not found.');
+    }
+}    
   
+  main();
