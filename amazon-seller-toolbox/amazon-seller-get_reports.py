8c56c16ccb37af17dc5d1711939f028ff2b5a56d
Leonard Tulipan <leo@leotulipan.at>
2023-03-21 11:28:19 +0100

    find_element_by_id not working

---
+++ amazon-seller-toolbox/amazon-seller-get_reports.py
@@ -0,0 +1,40 @@
+# To get started, you need to install the Selenium package and Microsoft Edge WebDriver:
+
+# Install Selenium:
+# bash
+# Copy code
+# pip install selenium
+# Download the appropriate Microsoft Edge WebDriver from the following link: https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+# Make sure you download the WebDriver version that matches your installed version of Microsoft Edge.
+
+# Set the PATH environment variable to include the directory where you downloaded the Edge WebDriver.
+
+
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+
+# Path to the Edge WebDriver executable
+driver_path = "C:\\Users\\leo\\Downloads\\edgedriver\\msedgedriver.exe"
+
+# Create a new Edge service
+service = Service(executable_path=driver_path)
+
+# Create a new Edge browser instance and start it
+browser = webdriver.Edge(service=service)
+
+# Navigate to a specific URL
+browser.get("https://duckduckgo.com")
+
+# Locate the input element and send the desired text
+search_input = browser.find_element_by_id("search_form_input_homepage")
+search_input.send_keys("Python Selenium with Edge examples")
+
+# Option 1: Submit the form by calling the submit method on the input element
+search_input.submit()
+
+# Option 2: Locate the search button and click it to submit the form
+# search_button = browser.find_element_by_id("search_button_homepage")
+# search_button.click()
+
+# You can close the browser when you're done
+# browser.quit()
