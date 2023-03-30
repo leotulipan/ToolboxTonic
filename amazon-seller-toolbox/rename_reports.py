9cbdf4e767da9688fbaf44f409af8cc7ea01ebbb
Leonard Tulipan <leo@leotulipan.at>
2023-03-30 15:35:16 +0200

    Added Marketplace country to filename of reports

--- amazon-seller-toolbox/rename_reports.py
+++ amazon-seller-toolbox/rename_reports.py
@@ -4,28 +4,49 @@ import shutil                          # Library for file operations
 import re                              # Module for regular expressions
 import sys                             # Module for working with the Python interpreter
 from dotenv import load_dotenv
+import re
+
+
+
+# Check if debug mode is specified
+# debug = False
+
+# Function to check if we are in debug mode
+def in_debug_mode():
+    # if debug == True:
+    #     return True
+    if len(sys.argv) > 1 and sys.argv[1] == "--debug":
+        # debug = True
+        return True
+    gettrace = getattr(sys, 'gettrace', None)
+    if gettrace is not None:
+        # debug = True
+        return True
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+if in_debug_mode():
+    clear_screen()
+    print("We are in debug mode.") 
 
 load_dotenv() # load environment variables from .env file
 
 directory = os.getenv("SOURCE_DIR")  # Source directory
 destination = os.getenv("DESTINATION_DIR")   # Destination directory
 
-# Check if debug mode is specified
-debug = False
-if len(sys.argv) > 1 and sys.argv[1] == "--debug":
-    debug = True
 
 matching_found = False                  # Initializes boolean variable to check if matching file is found
 for filename in os.listdir(directory):  # Looping through all files in the source directory
     # Check for filename matching pattern
     if re.match(r'^\d{9,15}\.txt$', filename):
-        if debug:
+        if in_debug_mode():
             print(f"Checking file for match: {filename}")
         start_date, end_date = '', ''
         # Open the file and read the first line for desired string
         with open(os.path.join(directory, filename), 'r') as f:
             if 'settlement-id' in f.readline():
-                if debug:
+                if in_debug_mode():
                     print(f"Processing file: {filename}")
                 matching_found = True   # Set flag for matching file found
 
@@ -41,11 +62,22 @@ for filename in os.listdir(directory):  # Looping through all files in the sourc
                     months_folder = [start_year_month, end_year_month]
                 else:
                     months_folder = [start_year_month]
+
+                # Get the marketplace out of Field 11 in line 3
+                marketplace_regex = re.compile(r'(?:^[^.]*\.)?([^.]*)(?:\s*|$)')
+                match = marketplace_regex.search(f.readline().split('\t')[11])
+                if match:
+                    marketplace = match.group(1).upper()
+                else:
+                    marketplace = ""
+    
+                if in_debug_mode():
+                    print("Marketplace: ", marketplace)
         
         # Rename the file with the new format and move it to the correct folder(s)
         if start_date != '' and end_date != '':
-            new_filename = f'{start_date} - {end_date} - {filename}'
-            if debug:
+            new_filename = f'{start_date} - {end_date} - {marketplace} - {filename}'
+            if in_debug_mode():
                 print(f"New Filename: {new_filename}")
             try:
                 os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
@@ -56,7 +88,7 @@ for filename in os.listdir(directory):  # Looping through all files in the sourc
             source_file_path = os.path.join(directory, new_filename)
 
             # Check if folder 0 exist and create it if needed
-            if debug:
+            if in_debug_mode():
                 print(f"Checking folder: {months_folder[0]}")
             if not os.path.exists(os.path.join(destination, months_folder[0])):
                 os.makedirs(os.path.join(destination, months_folder[0]))
@@ -68,7 +100,7 @@ for filename in os.listdir(directory):  # Looping through all files in the sourc
                 print(f"Skipping {filename} because it is currently in use by another process.")
 
             if len(months_folder) == 2:
-                if debug:
+                if in_debug_mode():
                     print(f"Checking folder: {months_folder[1]}")
                 # Check if folder 1 exist and create it if needed
                 if not os.path.exists(os.path.join(destination, months_folder[1])):
