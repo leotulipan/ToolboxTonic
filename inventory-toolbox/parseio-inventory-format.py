9fc4b62cac67a5eeee296b84bba59d7a779ddb8d
Leonard Tulipan <leo@leotulipan.at>
2023-04-07 11:09:20 +0200

    second inventory parse version with adapted regex

--- inventory-toolbox/parseio-inventory-format.py
+++ inventory-toolbox/parseio-inventory-format.py
@@ -4,7 +4,7 @@ import os
 import datetime
 
 # Regular expression pattern to match MHD date
-mhd_pattern = r'MHD\s+(\d{2}\.\d{2}\.\d{2}|\d{2}\.\d{4}|\d{2}\/\d{2})'
+mhd_pattern = r'MHD\s+(\d{2}\.\d{2}\.\d{2}|\d{2}\.\d{4}|\d{2}\/\d{2}|\d{2}\.\d{2}\.\d{4})'
 
 # Function to parse MHD date from article name
 def parse_mhd_date(article_name):
@@ -15,6 +15,8 @@ def parse_mhd_date(article_name):
             date = datetime.datetime.strptime(date_str, '%d.%m.%y').strftime('%Y-%m-%d')
         elif len(date_str) == 5:
             date = datetime.datetime.strptime(date_str, '%m/%y').strftime('%Y-%m-%d')
+        elif len(date_str) == 7:
+            date = datetime.datetime.strptime(date_str, '%d.%Y').strftime('%Y-%m-%d')
         else:
             date = datetime.datetime.strptime(date_str, '%d.%m.%Y').strftime('%Y-%m-%d')
         return date
@@ -36,27 +38,9 @@ for item in inventory_data[0]['inventar']:
     article = item['artikel']
     # Your code to split and extract article name and BBD date goes here
     # ...
-    m = re.match(r"(.+?)\s*(?:MHD:?|Haltbar bis:?|Best before:?|BB:?)?\s*([\d./]{6,10})(?:\s|\w|$)", article)
+    m = re.match(r"(.+?)\s*(?:MHD:?|Haltbar bis:?|Best before:?|BB:?)?\s*([\d./]{5,10})(?:\s|\w|$)", article)
     if m:
         name = m.group(1).strip()
         date = parse_mhd_date(article)
-        # date_str = m.group(2).replace('/', '.')
-        # if len(date_str) == 6:
-        #     date_str = f"{date_str[:2]}.20{date_str[4:]}"
-
-        # date = datetime.datetime.strptime(date_str, '%d.%m.%Y').strftime('%Y-%m-%d')
         print(name)
         print(date)
-    # print(article_name, bbd_date)
-
-
-# articles = [
-#         "KETO Coffee 5er MHD 28.05.2024",
-#         "KETO-CREAMER-CLASSIC Beutel MHD 15.07.24",
-#         "MCT- POWDER Beutel 01.03.23 abgelaufen!!!!!!",
-#         "KG-NUSS MHD 30.01.24 GT3032",
-#         "Brownie Chrisp Riegel (Vanille) violette Verpackung MHD: 15.02.24",
-#         "Theken Display NUSS „Trays“ 05.03.23 abgelaufen!!!!!!!",
-#         "BITES-HIMBEERE MHD 08.02.23Retoure abgelaufen!!!!!!!"
-# ]
-
